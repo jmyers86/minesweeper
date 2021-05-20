965d0cb505160a44f4eb308f4e4ab86210cfe3ce
Joe Myers <joecaricature@gmail.com>
2021-05-20 19:53:29 -0400

    Set up board and api queries

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [state, setState] = useState({
+    id: null,
+    board: [],
+    state: "new",
+    mines: 0,
+  });
+
+  // Set up useState Here
+  async function handleNewGameClick() {
+    const response = await fetch(
+      `https://minesweeper-api.herokuapp.com/games?difficulty=0`,
+      {
+        method: "POST",
+      }
+    );
+    const game = await response.json();
+    setState(game);
+  }
+
+  function handleCellClick(x, y) {
+    console.log(x, y);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Minesweeper</h1>
+      {state.id ? (
+        <div className="row">
+          {state.board.map((row, y) => (
+            <div>
+              {row.map((cell, x) => (
+                <div className="cell" onClick={() => handleCellClick(x, y)}>
+                  {cell}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <button className="new-game" onClick={handleNewGameClick}>
+          New Game!
+        </button>
+      )}
     </div>
   );
 }
